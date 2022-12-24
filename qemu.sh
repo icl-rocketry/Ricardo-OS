bcf68f9d3020877376b514b8a9b05b7292031c44
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2022-12-24 15:10:49 +0000

    More changes to the qemu script

--- qemu.sh
+++ qemu.sh
@@ -4,7 +4,7 @@
 # esptool.exe might be better off as esptool.py
 # Also maybe replace the paths to the .bin with whatever env you're using
 
-if [ $1 == "compile" ]
+if [ $1 == "link" ]
 then
     esptool.exe --chip esp32 \
         merge_bin --flash_mode dio --flash_size 4MB --fill-flash-size 4MB --output build/merged.bin \
@@ -17,13 +17,22 @@ then
     qemu-system-xtensa -nographic \
         -machine esp32 \
         -drive file=build/merged.bin,if=mtd,format=raw
-        -drive file=sd_image.bin,if=sd,format=raw
+        -global driver=timer.esp32.timg,property=wdt_disable,value=true
+        -drive file=build/sd_image.bin,if=sd,format=raw
 
 elif [ $1 == "debug" ]
 then
+    export QEMU_XTENSA_CORE_REGS_ONLY=1
     qemu-system-xtensa -nographic -s -S \
         -machine esp32 \
         -drive file=build/merged.bin,if=mtd,format=raw
-        -drive file=sd_image.bin,if=sd,format=raw
+        -global driver=timer.esp32.timg,property=wdt_disable,value=true
+        -drive file=build/sd_image.bin,if=sd,format=raw
+elif [ $1 == "attach" ]
+then
+    xtensa-esp32-elf-gdb .pio/build/esp32dev_idf4.4/firmware.elf \
+        -ex "target remote :1234" \
+        -ex "monitor system_reset" \
+        -ex "tb app_main" -ex "c"
 fi
 
